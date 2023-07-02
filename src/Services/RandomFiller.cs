ff98c043c7afb38f9f3149d086e62ca3f63cf1b8
Ramil Zaripov <zaripoff.ramil@gmail.com>
2023-07-02 16:20:10 +0500

    Now 4 can be generated

--- src/Services/RandomFiller.cs
+++ src/Services/RandomFiller.cs
@@ -6,13 +6,19 @@ namespace thegame.Services;
 
 public static class RandomFiller
 {
+    private static Random _random = new();
     public static bool Fill(CellDto[] cells)
     {
         var emptyTiles = cells.Where(c => c.Value == 0).ToList();
         if (emptyTiles.Count == 0) return false;
-        var rnd = new Random();
-        var next = rnd.Next(emptyTiles.Count);
-        emptyTiles[next].Value = 2;
+        var tileIndex = _random.Next(emptyTiles.Count);
+        emptyTiles[tileIndex].Value = TileValue();
         return true;
+    }
+
+    private static int TileValue()
+    {
+        var dice = _random.Next(10);
+        return dice <= 7 ? 2 : 4;
     }
 }
