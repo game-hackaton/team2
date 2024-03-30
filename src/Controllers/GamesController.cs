fedfb94b36b388897876da3c6536a09d9fc7edae
daHil <ssocmain@gmail.com>
2024-03-30 17:17:34 +0500

    added selection

--- src/Controllers/GamesController.cs
+++ src/Controllers/GamesController.cs
@@ -5,11 +5,11 @@ using thegame.Services;
 
 namespace thegame.Controllers;
 
-[Route("api/games")]
+[Route("api/games/{level}")]
 public class GamesController : Controller
 {
     [HttpPost]
-    public IActionResult Index()
+    public IActionResult Index([FromRoute]int level)
     {
         return Ok(TestData.AGameDto(new VectorDto {X = 2, Y = 2}, new Guid()));
     }
